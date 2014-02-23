200f014d7643b5b7637a102ba1dc589081de3756
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2014-02-23 21:34:36 +0100

    JSLinting

--- SampleApplication.Web/App/Angular/Caching/cacheInterceptor.js
+++ SampleApplication.Web/App/Angular/Caching/cacheInterceptor.js
@@ -1,15 +1,16 @@
-﻿'use strict';
+﻿(function () {
+    'use strict';
 
-angular.module('SampleApplication.Angular.Caching', ['SampleApplication.Config', 'SampleApplication.Common'])
-    .factory('SmartCacheInterceptor', ['$q', 'SampleApplicationVersion', function ($q, SampleApplicationVersion) {
-        return {
-            request: function (config) {
-                if (config.url.indexOf(".htm") > -1) {
-                    var separator = config.url.indexOf("?") === -1 ? "?" : "&";
-                    config.url = config.url + separator + "v=" + SampleApplicationVersion;
+    angular.module('SampleApplication.Angular.Caching', ['SampleApplication.Config', 'SampleApplication.Common'])
+        .factory('SmartCacheInterceptor', ['$q', 'SampleApplicationVersion', function ($q, SampleApplicationVersion) {
+            return {
+                request: function (config) {
+                    if (config.url.indexOf(".htm") > -1) {
+                        var separator = config.url.indexOf("?") === -1 ? "?" : "&";
+                        config.url = config.url + separator + "v=" + SampleApplicationVersion;
+                    }
+                    return config || $q.when(config);
                 }
-                return config || $q.when(config);
-            }
-        };
-    }]);
-;
+            };
+        }]);
+}());
