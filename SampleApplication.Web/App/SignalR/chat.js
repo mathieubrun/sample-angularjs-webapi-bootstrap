d63f126eb592da079600e18fdba37adf7754746c
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2014-03-10 21:34:16 +0100

    SignalR service test

--- SampleApplication.Web/App/SignalR/chat.js
+++ SampleApplication.Web/App/SignalR/chat.js
@@ -2,23 +2,36 @@
     'use strict';
 
     angular.module('SampleApplication.SignalR.Chat', ['ngResource', 'ngRoute'])
-        .controller('ChatController', ['$scope', '$resource', function ($scope, $resource) {
+        .service('signalRSvc', ['$rootScope', function ($rootScope) {
+
+            var connection = jQuery.hubConnection(),
+                contosoChatHubProxy = connection.createHubProxy('messagesHub'),
+                sendMessage = function (message) {
+                    contosoChatHubProxy.invoke('send', 'test', message);
+                };
+
+            contosoChatHubProxy.on('addNewMessageToPage', function (name, message) {
+                $rootScope.$emit("addNewMessageToPage", { name: name, message: message });
+            });
+
+            connection.start();
+
+            return {
+                sendMessage: sendMessage
+            };
+        }])
+        .controller('ChatController', ['$scope', '$rootScope', 'signalRSvc', function ($scope, $rootScope, signalRSvc) {
 
             $scope.messages = [];
 
             $scope.send = function (message) {
-                chat.server.send('test', message);
+                signalRSvc.sendMessage(message);
             };
 
-            var chat = $.connection.messagesHub;
-            // Create a function that the hub can call back to display messages.
-            chat.client.addNewMessageToPage = function (name, message) {
-                // Add the message to the page. 
-                $scope.messages.push({ name: name, message: message });
-            };
-
-            // Start the connection.
-            $.connection.hub.start().done(function () {
+            $rootScope.$on("addNewMessageToPage", function (e, msg) {
+                $scope.$apply(function () {
+                    $scope.messages.push(msg);
+                });
             });
-        }])
+        }]);
 }());
